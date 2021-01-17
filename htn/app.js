14539133523751e7ccbd0fa581ee317fc54f705d
Ryan Lee <ryanptss@gmail.com>
2021-01-17 01:57:59 -0800

    Added more sockets backend

--- htn/app.js
+++ htn/app.js
@@ -4,11 +4,11 @@ const http = require("http");
 const PORT = process.env.PORT || 4001;
 const index = require("./routes/index");
 const PTS_TO_WIN = 10;
+const NUM_OF_CARDS = 7;
 
 const app = express();
 const server = http.createServer(app);
 const GameManager = require("./controllers/GameManager");
-const { json } = require("express");
 app.use(index);
 
 const io = require("socket.io")(server, {
@@ -23,6 +23,9 @@ server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 // Game related
 
 const gameManager = new GameManager(PTS_TO_WIN);
+// put in video cards from firestore in below function
+gameManager.initializeVideoCards();
+gameManager.getVideoCards();
 // gameManager.newUser("ryan");
 // gameManager.newUser("marco");
 // gameManager.newUser("simon");
@@ -35,6 +38,8 @@ const gameManager = new GameManager(PTS_TO_WIN);
 // gameManager.nextJudge();
 // gameManager.nextJudge();
 // console.log(gameManager.getClientPlayerSlots());
+
+
 
 io.on("connection", socket => {
   // timer
@@ -57,17 +62,60 @@ io.on("connection", socket => {
   socket.on("request userData", () => {
     socket.emit("userListData", gameManager.getClientPlayerSlots());
   });
+  // when the game has officially started by someone pressing start game button
+  socket.on("start game", () => {
+    io.sockets.emit("game has started", "");
+  })
+  // after winner has been chosen and next round starts
+  socket.on("next round", () => {
+    io.sockets.emit("next round started", "");
+    io.sockets.emit("here is prompt", getRedditPrompt());
+  })
+  // called on componentDidMount when game has started
+  socket.on("get cards", () => {
+    if (gameManager.getIsFirstRound()) {
+      // give each person in clientplayerslots 7 cards;
+      let userCards = [];
+      for (let i = 0; i < NUM_OF_CARDS; i++) {
+        userCards.push(gameManager.popVideoCards());
+      }
+      socket.emit("7 userCards", userCards);
+    } else {
+      gameManager.getVideoCards();
+      // give each person in clientplayerslots 1 card;
+      socket.emit("1 userCard", gameManager.popVideoCards());
+    }
+  });
+  socket.on("choose winner", winnerSlotNumber => {
+    // increment a user's score, this one is only for the judge
+    gameManager.chooseWinner(winnerSlotNumber);
+    // broadcast to all clients who winner was
+    io.sockets.emit("winner", gameManager.getClientPlayerSlots);
+    // clear presentation video cards arrary for next round
+    gameManager.clearPresentationVideoCards();
+  });
+  socket.on("choose card", cardData => {
+    gameManager.pushPresentationVideoCards(cardData);
+    if (gameManager.getPresentationVideoCards().length === gameManager.getClientPlayerSlots().length) {
+      io.sockets.emit("everyone submitted", gameManager.getPresentationVideoCards());
+    }
+  });
 });
 
 // Reddit prompt creation
-const request = require('request')
+function getRedditPrompt() {
+  const request = require('request')
      ,url = 'https://www.reddit.com/r/funny/top/.json?count=20'
 
-request(url, (error, response, body)=> {
-  if (!error && response.statusCode === 200) {
-    const redditResponse = JSON.parse(body)["data"]["children"];
-    // redditResponse.forEach(obj => console.log(obj["data"]["title"]))
-  } else {
-    console.log("Got an error: ", error, ", status code: ", response.statusCode);
-  }
-});
+  request(url, (error, response, body)=> {
+    if (!error && response.statusCode === 200) {
+      const redditResponse = JSON.parse(body)["data"]["children"];
+      let redditPrompts = [];
+      redditResponse.forEach(obj => redditPrompts.push(obj["data"]["title"]));
+      return redditPrompts;
+    } else {
+      console.log("Got an error: ", error, ", status code: ", response.statusCode);
+    }
+  });
+}
+
--- htn/controllers/GameManager.js
+++ htn/controllers/GameManager.js
@@ -4,6 +4,9 @@ class GameManager {
         this.whoIsJudge = null;
         this.isGameOver = false;
         this.clientPlayerSlots = new Array(10).fill(null);
+        this.videoCards = [];
+        this.presentationVideoCards = [];
+        this.isFirstRound = true;
     }
 
     newUser(name) {
@@ -18,6 +21,10 @@ class GameManager {
 
     removeUser(slotNumber) {
         this.clientPlayerSlots[slotNumber-1] = null;
+    }
+
+    initializeVideoCards(videoCards) {
+        this.videoCards = videoCards;
     }
 
     chooseWinner(slotNumber) {
@@ -54,6 +61,49 @@ class GameManager {
     getClientPlayerSlots() {
         return this.clientPlayerSlots;
     }
+
+    getVideoCards() {
+        return shuffleCards(this.videoCards);
+    }
+
+    popVideoCards() {
+        return this.videoCards.pop();
+    }
+
+    getPresentationVideoCards() {
+        return this.presentationVideoCards;
+    }
+
+    pushPresentationVideoCards(card) {
+        this.presentationVideoCards.push(card);
+    }
+
+    clearPresentationVideoCards() {
+        this.presentationVideoCards = [];
+    }
+
+    getIsFirstRound() {
+        return this.isFirstRound;
+    }
+
+    shuffleCards(array) {
+        var currentIndex = array.length, temporaryValue, randomIndex;
+      
+        // While there remain elements to shuffle...
+        while (0 !== currentIndex) {
+      
+          // Pick a remaining element...
+          randomIndex = Math.floor(Math.random() * currentIndex);
+          currentIndex -= 1;
+      
+          // And swap it with the current element.
+          temporaryValue = array[currentIndex];
+          array[currentIndex] = array[randomIndex];
+          array[randomIndex] = temporaryValue;
+        }
+      
+        return array;
+      }
 }
 
 module.exports = GameManager
